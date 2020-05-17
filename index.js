7340311a40d3ca8abb791b5825a44ab01bc743a5
Demetrio <dem9411@gmail.com>
2020-05-17 00:59:17 -0500

    Agregue rama books

--- index.js
+++ index.js
@@ -7,19 +7,28 @@ const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./database.sqlite"
 });
+
 /**MODELO */
-const Usuario = sequelize.define("usuario", {
+const Libro = sequelize.define("libro", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     primaryKey: true,
     unique: true
   },
-  nombre: {
+  titulo: {
     type: Sequelize.STRING,
     allowNull: false
   },
-  apellido: {
+  autor: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  editorial:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  fecha: {
     type: Sequelize.STRING,
     allowNull: false
   }
@@ -34,43 +43,46 @@ const app = express();
 app.use(bodyParser.json());
 
 /**RUTAS */
-app.get("/usuarios", async (req, res) => {
-  const todosUsuarios = await Usuario.findAll();
-  res.json(todosUsuarios);
+app.get("/libros", async (req, res) => {
+  const todoslibros = await Libro.findAll();
+  res.json(todoslibros);
 });
-app.get("/usuarios/:id", async (req, res) => {
+
+app.get("/libros/:id", async (req, res) => {
   const id = req.params.id;
-  const unUsuario = await Usuario.findAll({
+  const unLibro = await Libro.findAll({
     where: {
       id: id
     }
   });
-  res.json(unUsuario);
-});
-app.post("/usuarios", async (req, res) => {
-  const { nombre, apellido } = req.body;
-  const nuevoUsuario = await Usuario.create({ nombre, apellido });
-  res.json(nuevoUsuario);
+  res.json(unLibro);
 });
 
-//TODO: EL PUT y EL DELETE
+app.post("/libros", async (req, res) => {
+  const { titulo, autor, editorial, fecha } = req.body;
+  const nuevoLibro = await Libro.create({titulo, autor, editorial, fecha });
+  res.json(nuevoLibro);
+});
 
-app.put("/usuarios/modificar/:id", async (req, res) => {
+
+// TODO: EL PUT y EL DELETE
+
+app.put("/libros/modificar/:id", async (req, res) => {
   const { id } = req.params; //el id esta en la url, lo guardamos en una constante con el mismo nombre
-  const { nombre, apellido } = req.body; //el nombre y el apellido van a salir del json, es decir esta en req.body
-  const usuarioModificado = await Usuario.update(
-    { nombre, apellido },
+  const { titulo, autor, editorial, fecha } = req.body; //el nombre y el apellido van a salir del json, es decir esta en req.body
+  const libroModificado = await Libro.update(
+    { titulo, autor, editorial, fecha  },
     { where: { id: id } }
   ); // cambiamos al usuario donde el id coincida con el id de params.
   // es lo mismo escribir const id = req.params.id y const {id} = req.params
-  res.json(usuarioModificado); //respondemos con un json que tiene el usuario modificado
+  res.json(libroModificado); //respondemos con un json que tiene el usuario modificado
 });
 
-app.delete("/usuarios/eliminar/:id", async (req, res) => {
+
+app.delete("/libros/eliminar/:id", async (req, res) => {
   const { id } = req.params;
-  await Usuario.destroy({ where: { id } }); // es lo mismo poner {id: id} a {id}
-  res.json({ mensaje: "Usuario Eliminado" });
-});
-
+  await Libro.destroy({ where: { id } }); // es lo mismo poner {id: id} a {id}
+  res.json({ mensaje: "Libro Eliminado" });
+}); 
 /**APP ESCUCHA PUERTO */
-app.listen(3456, () => console.log("http://localhost:3456/usuarios"));
+app.listen(3456, () => console.log("http://localhost:3456/libros"));
